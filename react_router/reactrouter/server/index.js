ef078568942d32aece12ca83c4b210361ce2e1ab
ansonhwo <ahwong19@gmail.com>
2017-05-11 19:06:30 -0700

    Removing meteor and react-router projects

--- react_router/reactrouter/server/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-
-const PORT = process.env.PORT || 9999
-
-app.use(express.static(__dirname + '/public'))
-
-app.listen(PORT, () => console.log(`Listening on ${PORT}`))
